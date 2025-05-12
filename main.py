74461440884dd74dee379b9dfff12f9d89271c2c
Akshay <n.akshay.kanna@gmail.com>
2025-05-13 00:35:35 +0530

    Speech module

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import speech_recognition as sr
+import os
+import pyttsx3
 
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source :
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            query = r.recognize_google(audio,language = "en-in")
+            print(f"User said {query}")
+            return query
+        except Exception as e:
+            return "Some error occured,Sorry from Jarvis"
 
 if __name__ == "__main__" :
-    print("Hello World !!")
+    '''speak("Hello World !!")
+    speak("Hello I am Jarvis A I")'''
+    while True:
+        print("Listening...")
+        query = take_command()
+        #speak(query)
---
+++ speak.py
@@ -0,0 +1,7 @@
+import win32com.client
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+while(1):
+    print("Enter the word you want to speak:")
+    s = input()
+    speaker.speak(s)
